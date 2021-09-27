7ea7d88e52bf0c01332e83579e2c5ca408a23e37
Luna <luna@lunaluna.me>
2021-09-27 23:01:35 +0100

    Initial commit

---
+++ src/main/kotlin/me/lunaluna/spigot/exampleplugin/ExamplePlugin.kt
@@ -0,0 +1,8 @@
+package me.lunaluna.spigot.exampleplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ExamplePlugin : JavaPlugin() {
+    override fun onEnable() { /* Plugin startup logic */ }
+    override fun onDisable() { /* Plugin shutdown logic */ }
+}
