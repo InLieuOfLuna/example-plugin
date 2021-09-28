ba8d90990c8946d7dee673307d311ad42f570688
Luna <luna@lunaluna.me>
2021-09-28 23:19:00 +0100

    Added a listener to the plugin

--- src/main/kotlin/me/lunaluna/spigot/exampleplugin/ExamplePlugin.kt
+++ src/main/kotlin/me/lunaluna/spigot/exampleplugin/ExamplePlugin.kt
@@ -1,8 +1,11 @@
 package me.lunaluna.spigot.exampleplugin
 
+import me.lunaluna.spigot.exampleplugin.listeners.ExampleListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class ExamplePlugin : JavaPlugin() {
-    override fun onEnable() { /* Plugin startup logic */ }
-    override fun onDisable() { /* Plugin shutdown logic */ }
+    override fun onEnable() {
+        ExampleListener(this)
+    }
+    override fun onDisable() { }
 }
---
+++ src/main/kotlin/me/lunaluna/spigot/exampleplugin/listeners/ExampleListener.kt
@@ -0,0 +1,17 @@
+package me.lunaluna.spigot.exampleplugin.listeners
+
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerBedEnterEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class ExampleListener(plugin: JavaPlugin) : Listener {
+
+    init { Bukkit.getPluginManager().registerEvents(this, plugin) }
+
+    @EventHandler
+    fun onTick(event: PlayerBedEnterEvent) {
+        event.player.damage(1.0)
+    }
+}
